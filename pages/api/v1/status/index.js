bcb37cd3a66842c122f45fb2478f6fab4c686435
Pablo HSC <pablo.h.silva.cunha@gmail.com>
2025-07-13 20:06:18 -0300

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 100 + 1 as soma");
+  console.log(result.rows[0].soma);
   response.status(200).json({ chave: "são acima da média" });
 }
 
