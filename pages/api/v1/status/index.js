34105c0d5bb2c0459a5e30fc4721da5cd8601d57
Pablo HSC <pablo.h.silva.cunha@gmail.com>
2025-07-16 06:09:51 -0300

    add `jsconfig.json` file

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js";
 
 async function status(request, response) {
   const result = await database.query("SELECT 100 + 1 as soma");
-  console.log(result.rows[0].soma);
+  console.log(result.rows[0]);
   response.status(200).json({ chave: "são acima da média" });
 }
 
