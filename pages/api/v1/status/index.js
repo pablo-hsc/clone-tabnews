4d7679665bf4ef37df98938942f1c3f05b6b1fed
Pablo HSC <pablo.h.silva.cunha@gmail.com>
2025-08-07 22:10:50 -0300

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,40 @@
 import database from "infra/database.js";
+import { VALID_LOADERS } from "next/dist/shared/lib/image-config";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 100 + 1 as soma");
-  console.log(result.rows[0]);
-  response.status(200).json({ chave: "são acima da média" });
+  const updatedAt = new Date().toISOString();
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections",
+  );
+  const dataBaseMaxConnectionsValue = parseInt(
+    databaseMaxConnectionsResult.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+
+  const dataBaseOpenedConnectionsResult = await database.query({
+    text: `SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;`,
+    values: [databaseName],
+  });
+
+  const dataBaseOpenedConnectionsValue =
+    dataBaseOpenedConnectionsResult.rows[0].count;
+
+  console.log(dataBaseOpenedConnectionsValue);
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: dataBaseMaxConnectionsValue,
+        opened_connections: dataBaseOpenedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
